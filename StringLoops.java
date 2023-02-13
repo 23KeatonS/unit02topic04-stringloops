ce09663863e72cce90fb19b61bc9138b6d8dbdc1
Keaton Stwalley <23keatonstwalley@usd266.com>
2023-02-13 11:40:58 -0600

    added reverseWords and repeatedConsecs

--- StringLoops.java
+++ StringLoops.java
@@ -29,7 +29,31 @@ public class StringLoops {
     return indexList;
   }
 
+  public static boolean repeatedConsec(String str){
+    
+    for (int i = 0; i<str.length()-1; i++){
+      if (str.substring(i,i+1).equals(str.substring(i+1,i+2))){
+        return true;
+      }
+    }
+    
+    return false;
+  }
+
+public static String reverseWords(String str){
   
+  String newStr = "";
+  int endIndex = str.length();
+  for (int i = str.length()-1; i>0; i--){
+    if (str.substring(i-1,i).equals(" ")){
+      newStr += str.substring(i,endIndex)+" ";
+      endIndex = i-1;
+    }
+  }
+  newStr += str.substring(0,str.indexOf(" "));
+  return newStr;
+    
+}
 
 
   public static void main(String[] args) {
@@ -37,6 +61,7 @@ public class StringLoops {
     // testing of static methods goes here
     System.out.println(reverseCharacter("cat"));
     System.out.println(Arrays.toString(indexesOfAll("attack", "a")));
-
+    System.out.println(repeatedConsec("hello"));
+    System.out.println(reverseWords("taco cat"));
   }
 }
